c444a790c94d35ca92e3bcc33b41cefa0e7c6c04
wmylxmj <1397322329@qq.com>
2026-05-12 20:41:29 +0800

    搭建HDR框架

--- VulkanPBR/Scene.cpp
+++ VulkanPBR/Scene.cpp
@@ -1,6 +1,9 @@
 #include "Scene.h"
 
 #include "VulkanUtils.h"
+#include "FrameBuffer.h"
+
+FrameBufferEx* g_HdrFrameBuffer = nullptr;
 
 void OnViewportChanged(int inWidth, int inHeight)
 {
@@ -9,8 +12,22 @@ void OnViewportChanged(int inWidth, int inHeight)
 
 void InitScene()
 {
+	GlobalConfig& globalConfig = GetGlobalConfig();
+	g_HdrFrameBuffer = new FrameBufferEx();
+	g_HdrFrameBuffer->SetSize(globalConfig.viewportWidth, globalConfig.viewportHeight);
+	g_HdrFrameBuffer->AttachColorBuffer(VK_FORMAT_R32G32B32A32_SFLOAT);
+	g_HdrFrameBuffer->AttachDepthBuffer();
+	g_HdrFrameBuffer->Finish(); // Shader Resource View
 }
 
 void RenderOneFrame()
 {
+	// Render Scene to HDR FrameBuffer
+	VkCommandBuffer commandBuffer = g_HdrFrameBuffer->BeginRendering(); // HDR Render Pass
+
+	vkCmdEndRenderPass(commandBuffer);
+	// Render HDR to Swapchain Image
+	BeginRendering(commandBuffer); // Swapchain Render Pass
+	EndRendering();
+	SwapBuffers();
 }
